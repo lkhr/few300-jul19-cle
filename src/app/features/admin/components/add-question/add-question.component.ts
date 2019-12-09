ced5bbce92f990f37029865771c2ff053d0afffc
lkhr <lkhodge0723@gmail.com>
2019-12-09 19:11:40 +0000

    lab add question done

--- src/app/features/admin/components/add-question/add-question.component.ts
+++ src/app/features/admin/components/add-question/add-question.component.ts
@@ -3,6 +3,7 @@ import { Store } from '@ngrx/store';
 import { MathState } from 'src/app/features/math/reducers';
 import { AddQuestionModel } from 'src/app/features/math/models';
 import * as actions from '../../../math/actions/questions.actions';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-add-question',
@@ -10,7 +11,7 @@ import * as actions from '../../../math/actions/questions.actions';
   styleUrls: ['./add-question.component.scss']
 })
 export class AddQuestionComponent implements OnInit {
-
+  name = new FormControl('');
   @Input() addQuestionModel: AddQuestionModel = {
     question: '',
     answer: 0
